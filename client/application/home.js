bd8d3f1364935535f18b12e6f239cf9148cf9071
nyletara <iguru95@gmail.com>
2016-12-24 03:07:32 +0530

    Shows who's going with total count

--- client/application/home.js
+++ client/application/home.js
@@ -3,6 +3,8 @@
  */
 
 import { Events } from '../../collections/events.js';
+import { CurrentUsers } from '../../collections/users.js';
+
 
 Template.home.onCreated(function() {
     console.log(Meteor.user());
@@ -20,8 +22,38 @@ Template.home.helpers({
     },
 
     userGoing : function (users) {
-        console.log(users);
         return users.includes(Meteor.userId());
+    },
+
+    usersFunction : function (users) {
+        console.log(users.length);
+        var whosgoing = "Who's going? ";
+        if (users.length > 1) {
+            for (var i = 0; i < users.length; i++) {
+                console.log(users[i]);
+                if (users[i] != "temp") {
+                    var host = (CurrentUsers.find({userId: users[i]}).fetch())[0].name;
+                    console.log(host);
+                    if (host != null) {
+                        whosgoing = whosgoing + host + " ";
+                    }
+                }
+            }
+            console.log(whosgoing);
+        }
+        return whosgoing;
+    },
+
+    totalUsers : function (users) {
+        console.log(users.length);
+        if (users.length > 1) {
+            for (var i = 0; i < users.length; i++) {
+                console.log(users[i]);
+                if (users[i] != "temp") {
+                    return users.length-1;
+                }
+            }
+        }
     }
 });
 
